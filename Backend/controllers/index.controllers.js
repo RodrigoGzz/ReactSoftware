07e68deb0190774bbf3cf64e188a75a2d2aef35b
RodrigoGzz <a00837024@tec.mx>
2025-02-24 13:36:45 -0600

    wa

--- Backend/controllers/index.controllers.js
+++ Backend/controllers/index.controllers.js
@@ -1,2 +1,2 @@
-export const getIndex = (req, res) => res.send("Hello, World from API!");
+export const getIndex = (req, res) => res.send("Hello, World from API!!!");
 export const getPing = (req, res) => res.send("Pong!");
