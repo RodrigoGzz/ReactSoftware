1b06ff09b617b5957a748f96b7034102d86f7e78
RodrigoGzz <a00837024@tec.mx>
2025-02-27 12:48:15 -0600

    Cambios

--- Backend/controllers/items.controllers.js
+++ Backend/controllers/items.controllers.js
@@ -4,3 +4,10 @@ export const getItems = async () => {
     const pool = await sqlConnect();
     const data = await pool.request().query("select * from items");
 };
+
+export const getItem = async () => {
+    const pool = await sqlConnect();
+    const data = await pool.request().input("", sql.varChar, req.parameters.id).query("insert into items (name,price) values (@name, @price)");
+    res.status(200).json({operation: true});
+
+};
--- Backend/routes/items.routes.js
+++ Backend/routes/items.routes.js
@@ -3,5 +3,6 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/items/", getItems);
+router.get("/items/:id", getItem);
 
 export default router;
