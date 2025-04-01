dfd1627d04674771adbba0c6839e4fd652b39607
RodrigoGzz <117121154+RodrigoGzz@users.noreply.github.com>
2025-03-31 18:58:07 -0600

    Cambios

--- Backend/controllers/index.controllers.js
+++
@@ -1,2 +0,0 @@
-export const getIndex = (req, res) => res.send("Hello, World from API!!!");
-export const getPing = (req, res) => res.send("Pong!");
--- Backend/routes/items.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-router.get("/items/", getItems);
-router.get("/items/:id", getItem);
-
-export default router;
