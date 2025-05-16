bf4e14b63551a3ab806648a26b147f2dd2b89904
RodrigoGzz <a00837024@tec.mx>
2025-05-16 10:17:18 -0600

    vercel

--- Backend/routes/users.routes.js
+++ Backend/routes/users.routes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get("/", getUsers);
 router.get("/:id", getUser);
-router.post("/", postUser);
+router.post("/signup", postUser);
 router.put("/:id", putUser);
 router.delete("/:id", deleteUser);
 
