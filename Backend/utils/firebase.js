b6671cba967c6c7c709c1aef6f1ba0adb112b3e2
RodrigoGzz <a00837024@tec.mx>
2025-05-16 09:52:55 -0600

    Vercel

--- Backend/utils/firebase.js
+++ Backend/utils/firebase.js
@@ -1,10 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import fs from "fs";
-import path from "path";
 
-const firebaseKeyPath = path.resolve("utils/firebasekey.json");
-const firebasekey = JSON.parse(fs.readFileSync(firebaseKeyPath, "utf-8"));
+const firebasekey = JSON.parse(process.env.FIREBASE_CONFIG);
 
 const app = initializeApp(firebasekey);
 export const db = getFirestore(app);
